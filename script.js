a81b30f8ad01b743dad08bc63d937df617a190ff
Syming2002 <simon.bruneau@yahoo.com>
2026-02-10 11:02:12 +0100

    ajout pagination

--- script.js
+++ script.js
@@ -62,3 +62,64 @@ buttonApply.addEventListener("click", searchAndSortProducts)
 
 displayProducts(products);
 
+const text = document.getElementById('text');
+const pagination = document.getElementById('pagination');
+
+
+
+// On récupère tout le texte initial sans espace
+const fullText = text.textContent.trim();
+
+// On découpe en mots pour éviter de couper les mots au milieu
+const words = fullText.split(/\s+/);
+
+// Nombre de mots par page
+const wordsPerPage = 300;
+
+const totalPages = Math.ceil(words.length / wordsPerPage);
+let currentPage = 1;
+
+// fonction qui affiche la page précise du texte
+function showPage(page) {
+    // if (!text || !pagination) return;
+
+    if (page < 1) page = 1;
+    if (page > totalPages) page = totalPages;
+    
+    currentPage = page;
+
+    const start = (currentPage - 1) * wordsPerPage;
+    const end = start + wordsPerPage;
+    const pageWords = words.slice(start, end);
+
+    text.textContent = pageWords.join(' ');
+
+    renderPagination()
+}
+
+
+function renderPagination() {
+    pagination.innerHTML = '';
+
+    // btn précédent
+    const prevBtn = document.createElement('button')
+    prevBtn.textContent = '← Précédent'
+    prevBtn.disabled = currentPage === 1;
+
+    prevBtn.onclick = () => showPage(currentPage - 1)
+    pagination.appendChild(prevBtn);
+
+    // Éviter afficher tout les numéros des pages
+    const info = document.createElement("span")
+    info.textContent = `Page ${currentPage} / ${totalPages}`;
+    pagination.appendChild(info);
+
+    // btn page suivante
+    const nextBtn = document.createElement("button");
+    nextBtn.textContent = "Suivant →";
+    nextBtn.disabled = currentPage === totalPages;
+    nextBtn.onclick = () => showPage(currentPage + 1)
+    pagination.appendChild(nextBtn);
+}
+
+showPage(1);
